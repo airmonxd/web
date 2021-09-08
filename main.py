09927b59bea151f52ce6dabc1fd89b02d2cc1e20
Airmon <53837488+airmonxd@users.noreply.github.com>
2021-09-08 00:50:07 +0000

    Everything

--- main.py
+++
@@ -1,9 +0,0 @@
-from sanic import Sanic
-from sanic.response import text
-
-app = Sanic("MyHelloWorldApp")
-app.static("/","src/index.html")
-app.static("/","src/")
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000, debug=False)
